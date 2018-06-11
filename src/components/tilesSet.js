2dabef54fc4ffb898663adbd80a3b43a2bab3065
Dmytro Davydov <dd77.ua@gmail.com>
2018-06-11 21:42:15 +0300

    randomizeContent

--- src/components/tilesSet.js
+++ src/components/tilesSet.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import Tile from './tile';
 
 class TilesSet extends Component {
+	constructor(props) {
+		super(props);
+
+		this.tilesContArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+		this.randContArr = [];
+		this.counter = 0;
+	}
+
+	randomizeContent() {
+		const randomNum = Math.floor(Math.random()*8);
+		const usedElemLength =
+			this.randContArr.filter(key => {return key === this.tilesContArr[randomNum]}).length;
+		if (this.counter <= 16 && usedElemLength < 2) {
+			this.randContArr.push(this.tilesContArr[randomNum]);
+			this.counter++;
+		}
+		console.log('this.usedElements.length', this.randContArr);
+	}
 
 	render() {
 
-		const tilesArr = [];
-
-		for (let i = 1; i <= 16; i ++) {
-			tilesArr.push(
-				<Tile content='content' />
-			);
+		while (this.counter < 16) {
+			this.randomizeContent();
 		}
+
+		const tilesArr = this.randContArr.map(key => {
+			return (
+				<div className='tile'>
+					<Tile content={key} />
+				</div>
+			);
+		});
 
 		return(
 			<div className='tiles_wrapper'>
